c8b48930ef0954d2649be22b057c03f67d10155f
Luis Henrique Facunde da Silva <luis.silva@FAV.COM.BR>
2025-07-02 10:18:35 -0300

    feat: primeiro commit - estruturação do diretório da aplicação.

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+export const routes = [
+    {
+        method: 'POST',
+        path: '/tasks',
+        handler: (req, res) => {}
+    },
+    {
+        method: 'GET',
+        path: '/tasks',
+        handler: (req, res) => {}
+    },
+    {
+        method: 'PUT',
+        path: '/tasks/:id',
+        handler: (req, res) => {}
+    },
+    {
+        method: 'DELETE',
+        path: '/tasks/:id',
+        handler: (req, res) => {}
+    },
+    {
+        method: 'PATCH',
+        path: '/tasks/:id',
+        handler: (req, res) => {}
+    }
+]
